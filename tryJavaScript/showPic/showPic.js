fae233b32f50902a75cac6694a682e8679e1630e
montehuang <hqzmonte@sina.com>
2018-02-07 01:50:16 +0800

    动态添加元素节点和文本节点和属性等

--- tryJavaScript/showPic/showPic.js
+++ tryJavaScript/showPic/showPic.js
@@ -1,24 +1,70 @@
 function showPic(node){
+    if(!document.getElementById("placeholder")) return true;
     var source = node.getAttribute("href");
-    var img = document.getElementById("description");
+    var img = document.getElementById("placeholder");
     img.setAttribute("src", source);
-    var text = node.getAttribute("title");
-    var placeholder = document.getElementById("placeholder");
-    placeholder.firstChild.nodeValue = text;
+    if(!document.getElementById("description")) return false;
+    var text = node.getAttribute("title") ? node.getAttribute("title") : "";
+    var description  = document.getElementById("description");
+    description.firstChild.nodeValue = text;
+    return false;
+}
+
+function addLoadEvent(func){
+    var oldload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder(){
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imageGallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("src", "images/default.jpg");
+    placeholder.setAttribute("width", "300");
+    placeholder.setAttribute("height", "200");
+    placeholder.setAttribute("id", "placeholder");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    description.appendChild(document.createTextNode("Choose a picutre"));
+    var gallery = document.getElementById("imageGallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
 }
 
 function popUp(url){
     window.open(url, 'popup', 'width=620,height=480')
 }
-window.onload = function(){
+function prepareGallery(){
     if(!document.getElementsByTagName) return false;
-    var items = document.getElementsByTagName('a');
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imageGallery")) return false;
+    var gallery = document.getElementById("imageGallery");
+    var items = gallery.getElementsByTagName('a');
     for(var i = 0; i < items.length; i++){
-        if(items[i].getAttribute('class') == 'popup'){
-            items[i].onclick = function(){
-                popUp(this.getAttribute('href'));
-                return false;
-            }
+        items[i].onclick = function(){
+            return showPic(this);
         }
+        items[i].onkeypress = items[i].onclick;
     }
 }
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
